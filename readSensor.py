58ab6ab070979069b8390c2d841933d7746952f4
Daichi Hayashi <daichikuwa0618@gmail.com>
2017-09-15 14:26:24 +0900

    add ServoMotor

--- readSensor.py
+++ readSensor.py
@@ -2,14 +2,38 @@
 #!/usr/bin/env python
 
 # read analog photo reflector sensor by using MCP3208 which has 8-channel analog to digital converter
+# move servo motor
+# 1500us = 0deg 900us = -60deg 2400us = 60deg
 
 from gpiozero import MCP3208
 from time import sleep
-adc = MCP3208(channel = 0)
+import pigpio
 
+adc = MCP3208(channel = 0)
+pi = pigpio.pi()
+
+pi.set_servo_pulsewidth(15, 1500)
+degree = 0
+microSec = 1500
 # loop for infinite
 while True:
-    print(adc.value)
-    sleep(0.5)
+    # 障害物に追従する
+    if adc.value > 0.5:
+        degree -=
+    elif adc.value < 0.5:
+        degree +=
+
+    if degree > 60:
+        degree = 60
+    elif degree < -60:
+        degree = -60
+
+    microSec = 1500 + (degree * 10)
+    pi.set_servo_pulsewidth(15, microSec)
+
+    print(str(degree) + "[deg], ADC: " + str(adc.value))
+
+    sleep(0.1)
+
 
 # end of program
