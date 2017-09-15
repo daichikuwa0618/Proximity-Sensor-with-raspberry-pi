3a5c2d137652958ae92104ff5702d1226fef7829
daichikuwa0618 <31601805+daichikuwa0618@users.noreply.github.com>
2017-09-15 15:58:18 +0900

    センサの値に応じてサーボモータの速度を制御できるように追加
    ロボットハンドに応用できるようにするため

--- readSensor.py
+++ readSensor.py
@@ -12,28 +12,74 @@ import pigpio
 adc = MCP3208(channel = 0)
 pi = pigpio.pi()
 
-pi.set_servo_pulsewidth(15, 1500)
-degree = 0
-microSec = 1500
+# ========== 定数 =========
+SERVO_PIN = 15
+GET_OBJECT = 0.8
+NEAR_OBJECT =0.3
+TOUCH_OBJECT = 0.1
+
+# ========== 変数 =========
+command = 0 # 0: OPEN 1: CLOSE
+state = 0 # distance between hand to object
+degree = 0.0
+microSec = 1500.0
+
+# initialize
+pi.set_servo_pulsewidth(SERVO_PIN, 1500)
+
 # loop for infinite
-while True:
-    # 障害物に追従する
-    if adc.value > 0.5:
-        degree -=
-    elif adc.value < 0.5:
-        degree +=
+try:
+    while True:
+        command = int(input("Type'1' to hand something: "))
+        # OPEN(60deg wait)
+        if command == 0:
+            pi.set_servo_pulsewidth(SERVO_PIN, 2400)
 
-    if degree > 60:
-        degree = 60
-    elif degree < -60:
-        degree = -60
+        # CLOSE(hand object)
+        else:
+            # max speed
+            while state == 0:
+                degree -= 1
+                microSec = 1500.0 + (degree * 10.0)
+                # set limit
+                if degree < -60.0:
+                    degree = -60.0
+                pi.set_servo_pulsewidth(SERVO_PIN, microSec)
+                print(str(degree) + "[deg], ADC: " + str(adc.value) + "state: " + str(state))
+                if adc.value < GET_OBJECT:
+                    # next stage
+                    state = 1
+                sleep(0.1)
 
-    microSec = 1500 + (degree * 10)
-    pi.set_servo_pulsewidth(15, microSec)
+            # middle speed
+            while state == 1:
+                degree -= 0.2
+                microSec = 1500.0 + (degree * 10.0)
+                # set limit
+                if degree < -60.0:
+                    degree = -60.0
+                pi.set_servo_pulsewidth(SERVO_PIN, microSec)
+                print(str(degree) + "[deg], ADC: " + str(adc.value) + "state: " + str(state))
+                if adc.value < NEAR_OBJECT:
+                    # next stage
+                    state = 2
+                sleep(0.1)
 
-    print(str(degree) + "[deg], ADC: " + str(adc.value))
 
-    sleep(0.1)
+            while state == 2:
+                degree -= 1
+                microSec = 1500.0 + (degree * 10.0)
+                # set limit
+                if degree < -60.0:
+                    degree = -60.0
+                pi.set_servo_pulsewidth(SERVO_PIN, microSec)
+                print(str(degree) + "[deg], ADC: " + str(adc.value) + "state: " + str(state))
+                if adc.value < TOUCH_OBJECT:
+                    # touch
+                    state = 0
+                sleep(0.1)
 
+except KeyboardInterrupt:
+    pass pi.set_servo_pulsewidth(SERVO_PIN, 0)
 
 # end of program
