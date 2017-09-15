dcb922965872baa594b37b81d8917cccb5f6c2ee
Daichi Hayashi <daichikuwa0618@gmail.com>
2017-09-15 09:49:10 +0900

    import関係

--- readSensor.py
+++ readSensor.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-#read analog photo reflector sensor by using MCP3208 which has 8-channel analog to digital converter
+# read analog photo reflector sensor by using MCP3208 which has 8-channel analog to digital converter
 
-import pizero
+from gpiozero import MCP3208
+adC = MCP3208(channel = 0)
+
+# end of program
