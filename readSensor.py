2734282d1bf0df2c9af06e2130ad0b310a45e8ab
Daichi Hayashi <daichikuwa0618@gmail.com>
2017-09-15 09:57:55 +0900

    フォトリフレクタのデータシートの追加

--- readSensor.py
+++ readSensor.py
@@ -4,6 +4,12 @@
 # read analog photo reflector sensor by using MCP3208 which has 8-channel analog to digital converter
 
 from gpiozero import MCP3208
+from time import sleep
 adC = MCP3208(channel = 0)
+
+# loop for infinite
+while True:
+    print(adc.value)
+    sleep(0.5)
 
 # end of program
